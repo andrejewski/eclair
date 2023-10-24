75d2ae54fcdfce555ea1326910f66a5e3d783b2c
Chris Andrejewski <christopher.andrejewski@gmail.com>
2023-10-24 06:52:59 -0400

    Replace T::Struct with POROs

--- lib/eclair/element.rb
+++ lib/eclair/element.rb
@@ -2,9 +2,25 @@
 require "sorbet-runtime"
 
 module Eclair
-  class Element < T::Struct
-    class DangerousUnescapedHtml < T::Struct
-      const :html, String
+  class Element
+    extend T::Sig
+    extend T::Helpers
+    final!
+
+    class DangerousUnescapedHtml
+      extend T::Sig
+      extend T::Helpers
+      final!
+
+      sig(:final) { returns(String) }
+      attr_reader :html
+
+      sig(:final) do
+        params(html: String).void
+      end
+      def initialize(html:)
+        @html = html
+      end
     end
 
     module Void
@@ -17,8 +33,22 @@ module Eclair
     ElementChild = T.type_alias { T.any(Element, String, DangerousUnescapedHtml) }
     Children = T.type_alias { T.any(T.class_of(Void), T::Array[ElementChild]) }
 
-    const :tag, Symbol
-    const :attributes, Attributes
-    const :children, Children
+    sig(:final) { returns(Symbol) }
+    attr_reader :tag
+
+    sig(:final) { returns(Attributes) }
+    attr_reader :attributes
+
+    sig(:final) { returns(Children) }
+    attr_reader :children
+
+    sig(:final) do
+      params(tag: Symbol, attributes: Attributes, children: Children).void
+    end
+    def initialize(tag:, attributes:, children:)
+      @tag = tag
+      @attributes = attributes
+      @children = children
+    end
   end
 end
